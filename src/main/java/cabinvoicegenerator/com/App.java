782f7cbfa696a3c996ec904059879076d0df0e13
nandinibaride <nandinibaride1234@gmail.com>
2021-10-01 14:59:10 +0530

    Add:.gitignore

---
+++ src/main/java/cabinvoicegenerator/com/App.java
@@ -0,0 +1,13 @@
+package cabinvoicegenerator.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
